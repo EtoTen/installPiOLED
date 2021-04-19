f060dba56dee030cd3af15d87530ea45d39fd94f
Nickolai Golubev <nick@uissoft.com>
2021-04-19 11:18:16 -0700

    Update stats.py

--- pioled/stats.py
+++ pioled/stats.py
@@ -24,12 +24,14 @@
 
 import time
 
-import Adafruit_SSD1306   # This is the driver chip for the Adafruit PiOLED
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
+from random import randrange
 
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-
 import subprocess
 
 
@@ -63,45 +65,45 @@ def get_gpu_usage():
         GPU = int(GPU)/10
     return GPU
 
-
 # 128x32 display with hardware I2C:
-# setting gpio to 1 is hack to avoid platform detection
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=None, i2c_bus=1, gpio=1)
+serial = i2c(port=1, address=0x3C)
+disp = sh1106(serial, width=128, height=64, rotate=0)
 
-# Initialize library.
-disp.begin()
+
+# Turn on display
+disp.show()
 
 # Clear display.
 disp.clear()
-disp.display()
 
 # Create blank image for drawing.
 # Make sure to create image with mode '1' for 1-bit color.
 width = disp.width
 height = disp.height
-image = Image.new('1', (width, height))
+image_stat = Image.new('1', (width, height))
 
 # Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+draw_stat = ImageDraw.Draw(image_stat)
 
 # Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
+draw_stat.rectangle((0, 0, width, height), outline=0, fill=0)
 
 # Draw some shapes.
 # First define some constants to allow easy resizing of shapes.
 padding = -2
 top = padding
 bottom = height-padding
+
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
 # Load default font.
 font = ImageFont.load_default()
 
-while True:
+def draw_stats():
 
     # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    draw_stat.rectangle((0, 0, width, height), outline=0, fill=0)
 
     # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "free -m | awk 'NR==2{printf \"Mem:  %.0f%% %s/%s M\", $3*100/$2, $3,$2 }'"
@@ -111,7 +113,7 @@ while True:
 
     # Print the IP address
     # Two examples here, wired and wireless
-    draw.text((x, top),       "eth0: " +
+    draw_stat.text((x, top),       "eth0: " +
               str(get_ip_address('eth0')),  font=font, fill=255)
     # draw.text((x, top+8),     "wlan0: " + str(get_ip_address('wlan0')), font=font, fill=255)
 
@@ -126,18 +128,84 @@ while True:
     if gpu_usage == 0.0:
         gpu_usage = 0.001
     draw_bar_width = int(full_bar_width*(gpu_usage/100))
-    draw.text((x, top+8),     "GPU:  ", font=font, fill=255)
-    draw.rectangle((x+string_width, top+12, x+string_width +
+    draw_stat.text((x, top+8),     "GPU:  ", font=font, fill=255)
+    draw_stat.rectangle((x+string_width, top+12, x+string_width +
                     draw_bar_width, top+14), outline=1, fill=1)
 
     # Show the memory Usage
-    draw.text((x, top+16), str(MemUsage.decode('utf-8')), font=font, fill=255)
+    draw_stat.text((x, top+16), str(MemUsage.decode('utf-8')), font=font, fill=255)
     # Show the amount of disk being used
-    draw.text((x, top+25), str(Disk.decode('utf-8')), font=font, fill=255)
+    draw_stat.text((x, top+25), str(Disk.decode('utf-8')), font=font, fill=255)
 
     # Display image.
-    # Set the SSD1306 image to the PIL image we have made, then dispaly
-    disp.image(image)
-    disp.display()
+    disp.display(image_stat)
     # 1.0 = 1 second; The divisor is the desired updates (frames) per second
     time.sleep(1.0/4)
+
+def init_stars(num_stars, max_depth):
+    stars = []
+    for i in range(num_stars):
+        # A star is represented as a list with this format: [X,Y,Z]
+        star = [randrange(-25, 25), randrange(-25, 25), randrange(1, max_depth)]
+        stars.append(star)
+    return stars
+
+
+def move_and_draw_stars(stars, max_depth):
+    origin_x = device.width // 2
+    origin_y = device.height // 2
+
+    with canvas(device) as draw:
+        for star in stars:
+            # The Z component is decreased on each frame.
+            star[2] -= 0.19
+
+            # If the star has past the screen (I mean Z<=0) then we
+            # reposition it far away from the screen (Z=max_depth)
+            # with random X and Y coordinates.
+            if star[2] <= 0:
+                star[0] = randrange(-25, 25)
+                star[1] = randrange(-25, 25)
+                star[2] = max_depth
+
+            # Convert the 3D coordinates to 2D using perspective projection.
+            k = 128.0 / star[2]
+            x = int(star[0] * k + origin_x)
+            y = int(star[1] * k + origin_y)
+
+            # Draw the star (if it is visible in the screen).
+            # We calculate the size such that distant stars are smaller than
+            # closer stars. Similarly, we make sure that distant stars are
+            # darker than closer stars. This is done using Linear Interpolation.
+            if 0 <= x < device.width and 0 <= y < device.height:
+                size = (1 - float(star[2]) / max_depth) * 4
+                if (device.mode == "RGB"):
+                    shade = (int(100 + (1 - float(star[2]) / max_depth) * 155),) * 3
+                else:
+                    shade = "white"
+                draw.rectangle((x, y, x + size, y + size), fill=shade)
+
+
+def main():
+    delay = 60 #60 second delay between stats and screensaver
+    max_depth = 32
+    stars = init_stars(512, max_depth)
+    t_end = time.time() + delay
+    screenSaver = False
+
+    while True:
+        if(time.time() > t_end):
+           t_end = time.time() + 60
+           screenSaver = not screenSaver
+
+        if(screenSaver):
+           move_and_draw_stars(stars, max_depth)
+        else:
+           draw_stats()
+
+if __name__ == "__main__":
+    try:
+        device = disp
+        main()
+    except KeyboardInterrupt:
+        pass
